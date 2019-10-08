ba40ba3addf7120870d00e7375b2b09cd480ac5e
songmin-gif <55427684+songmin-gif@users.noreply.github.com>
2019-10-08 15:30:52 +0900

    05project2

--- main.c
+++ main.c
@@ -4,13 +4,9 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-		int i=0,meter;
+	int i;
+	for (i=0; i<10; i++)
+		printf("Hello World!\n");
 	
-	while (i<3)
-	{
-		meter = i*1609;
-		printf("%d mile is %d meter.\n", i, meter);
-		i++;
-	}
 	return 0;
 }
