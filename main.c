084ec17ae34067732d6c0097ab94a4642ace4089
songmin-gif <55427684+songmin-gif@users.noreply.github.com>
2019-10-08 16:05:29 +0900

    05project3

--- main.c
+++ main.c
@@ -4,9 +4,19 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i;
-	for (i=0; i<10; i++)
-		printf("Hello World!\n");
+	int num=0;
+	char c;
 	
+	printf("input a string:");
+	
+	while ((c= getchar())!= '\n')
+	{
+		if (c <='9'&& c>='0') 
+		{
+			num++;
+		
+		}   
+	}
+	printf("%i\n", num);
 	return 0;
 }
